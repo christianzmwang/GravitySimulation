4ccedfb1eafd73d2962844621d6a038511470ac8
Christian Wang <58362194+christianzmwang@users.noreply.github.com>
2022-09-30 21:12:36 +0200

    Update prot0_1.py

--- prot0_1.py
+++ prot0_1.py
@@ -1,20 +1,11 @@
 
 
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-Create Your Own N-body Simulation (With Python)
-Philip Mocz (2020) Princeton Univeristy, @PMocz
-Simulate orbits of stars interacting due to gravity
-Code calculates pairwise forces according to Newton's Law of Gravity
-
-Edited to 2D
-"""
-
-
-def getAcc(pos, mass, G, softening):
+def getAcc(pos, mass, G):
     """
 Calculate the acceleration on each particle due to Newton's Law 
     pos  is an N x 3 matrix of positions
@@ -23,7 +14,7 @@ Calculate the acceleration on each particle due to Newton's Law
     softening is the softening length
     a is N x 3 matrix of accelerations
     """
-    # positions r = [x,y,z] for all particles
+    # positions r = [x,y] for all particles
     x = pos[:, 0:1]
     y = pos[:, 1:2]
 
@@ -32,7 +23,7 @@ Calculate the acceleration on each particle due to Newton's Law
     dy = y.T - y
 
     # matrix that stores 1/r^3 for all particle pairwise particle separations
-    inv_r3 = (dx**2 + dy**2 + softening**2)
+    inv_r3 = (dx**2 + dy**2)
     inv_r3[inv_r3 > 0] = inv_r3[inv_r3 > 0]**(-1.5)
 
     ax = G * (dx * inv_r3) @ mass
@@ -51,80 +42,89 @@ def main():
     t = 0      # current time of the simulation
     tEnd = 10.0   # time at which simulation ends
     dt = 0.01   # timestep
-    softening = 0.1    # softening length
     G = 1.0    # Newton's Gravitational Constant
     plotRealTime = True  # switch on for plotting as the simulation goes along
 
     # Generate Initial Conditions
     np.random.seed(17)  # set the random number generator seed
 
-    #Star, Planet, Planet, Meteor
-    mass = np.array([[36], [0.1]])  #20.0*np.ones((N, 1))/N   #total mass of particles is 20
+    # Speeds
+    spd = [10, 15, 20, 25, 30, 35, 40]
+
+    # Star, Planet
+    mass = np.array([[1000], [1]])  #20.0*np.ones((N, 1))/N   #total mass of particles is 20
     pos = np.array([[0.0, 0.0], [2.0, -3.0]])  # np.random.randn(N,3)   # randomly selected positions and velocities
-    vel = np.array([[0.0, 0.0], [0.0, 3.0]])  # np.random.randn(N,3)
-
-    # Convert to Center-of-Mass frame
-    vel -= np.mean(mass * vel, 0) / np.mean(mass)
-
-    # calculate initial gravitational accelerations
-    acc = getAcc(pos, mass, G, softening)
-
-    # number of timesteps
-    Nt = int(np.ceil(tEnd/dt))
-
-    # save energies, particle orbits for plotting trails
-    pos_save = np.zeros((N, 2, Nt+1))
-    pos_save[:, :, 0] = pos
-    t_all = np.arange(Nt+1)*dt
-
-    # prep figure
-    fig = plt.figure(figsize=(4, 5), dpi=80)
-    grid = plt.GridSpec(3, 1, wspace=0.0, hspace=0.3)
-    ax1 = plt.subplot(grid[0:2, 0])
-    ax2 = plt.subplot(grid[2, 0])
+    vel = np.array([[0.0, 0.0], [0.0, 0.0]])  # np.random.randn(N,3)
 
     # Simulation Main Loop
-    for i in range(Nt):
-        # (1/2) kick
-        vel += acc * dt/2.0
+    for i in spd: 
 
-        # drift
-        pos += vel * dt
+      vel[1] = [0.0, i]
+      pos[1] = [2.0, -3.0]
 
-        # update accelerations
-        acc = getAcc(pos, mass, G, softening)
+      # Convert to Center-of-Mass frame
+      vel[1] -= np.mean(mass * vel, 0) / np.mean(mass)
 
-        # (1/2) kick
-        vel += acc * dt/2.0
+      # calculate initial gravitational accelerations
+      acc = getAcc(pos, mass, G)
 
-        # update time
-        t += dt
+      # number of timesteps
+      Nt = int(np.ceil(tEnd/dt))
 
-        # save energies, positions for plotting trail
-        pos_save[:, :, i+1] = pos
+      # save energies, particle orbits for plotting trails
+      pos_save = np.zeros((N, 2, Nt+1))
+      pos_save[:, :, 0] = pos
+      t_all = np.arange(Nt+1)*dt
 
-        # plot in real time
-        if plotRealTime or (i == Nt-1):
-            plt.sca(ax1)
-            plt.cla()
-            xx = pos_save[:, 0, max(i-50, 0):i+1]
-            yy = pos_save[:, 1, max(i-50, 0):i+1]
-            plt.scatter(xx, yy, s=1, color=[.7, .7, 1])
-            plt.scatter(pos[:, 0], pos[:, 1], s=10, color='blue')
-            ax1.set(xlim=(-2, 2), ylim=(-2, 2))
-            ax1.set_aspect('equal', 'box')
-            ax1.set_xticks([i for i in range(-3, 6)])
-            ax1.set_yticks([i for i in range(-3, 4)])
+      # prep figure
+      fig = plt.figure(figsize=(4, 5), dpi=80)
+      grid = plt.GridSpec(3, 1, wspace=0.0, hspace=0.3)
+      ax1 = plt.subplot(grid[0:2, 0])
+      ax2 = plt.subplot(grid[2, 0])
 
-            plt.sca(ax2)
-            plt.cla()
-            ax2.set(xlim=(0, tEnd), ylim=(-1000, 1000))
-            ax2.set_aspect(0.0006)
+      for i in range(Nt):
+          # (1/2) kick
+          vel[1] += acc[1] * dt/2.0
 
-            plt.pause(0.0001)
+          # drift
+          pos[1] += vel[1] * dt
 
-        if pos[1, 0] <= 0:
-          break 
+          # update accelerations
+          acc = getAcc(pos, mass, G)
+
+          # (1/2) kick
+          vel[1] += acc[1] * dt/2.0
+
+          # update time
+          t += dt
+
+          # save energies, positions for plotting trail
+          pos_save[:, :, i+1] = pos
+
+          # plot in real time
+          if plotRealTime or (i == Nt-1):
+              plt.sca(ax1)
+              plt.cla()
+              xx = pos_save[:, 0, max(i-50, 0):i+1]
+              yy = pos_save[:, 1, max(i-50, 0):i+1]
+              plt.scatter(xx, yy, s=1, color=[.7, .7, 1])
+              plt.scatter(pos[:, 0], pos[:, 1], s=10, color='blue')
+              ax1.set(xlim=(-2, 2), ylim=(-2, 2))
+              ax1.set_aspect('equal', 'box')
+              ax1.set_xticks([i for i in range(-3, 6)])
+              ax1.set_yticks([i for i in range(-3, 4)])
+
+              plt.sca(ax2)
+              plt.cla()
+              ax2.set(xlim=(0, tEnd), ylim=(-1000, 1000))
+              ax2.set_aspect(0.0006)
+
+              plt.pause(0.0001)
+
+          if pos[1, 0] <= 0:
+
+            break 
+
 
     print("hey")
 
